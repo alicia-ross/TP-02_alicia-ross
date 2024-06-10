250957065bed84cfedcbcaeffa759bbede1c3139
alicia ross <ali.ross14@outlook.com>
2024-06-10 17:38:10 -0400

    'first commit'

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,html}",
+  ],
+  theme: {
+    colors: {
+      'purple': '#642eff',
+      'darkpurple': '#3730a3',
+      'white': '#FFFFFF'
+    },
+    fontFamily: {
+      'sans' : ['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
