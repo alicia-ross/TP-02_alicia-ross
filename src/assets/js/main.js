5106d2fbae3d21ed3ef958d956efc5e14e8379e7
alicia ross <ali.ross14@outlook.com>
2024-07-09 11:30:22 -0400

    final

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,9 @@
 import '../css/style.css';
 
 class Card {
-    constructor(numero, size) {
+    constructor(title, numero, size) {
+        /**@type {string} */
+        this.title = title;
         /** @type {string} */
         this.numero = numero;
         /** @type {string} */
@@ -16,6 +18,9 @@ class Card {
         /** @type {HTMLDivElement} */
         const div = document.createElement('div');
         div.classList.add('card', 'bg-black', 'flex', 'gap-3', 'flex-col', 'text-white', 'justify-center', 'items-center', 'p-3', 'text-base', 'lg:text-lg', 'rounded', 'w-[200px]', 'h-[200px]', 'lg:w-[300px]', 'lg:h-[300px]');
+
+        const title = document.createElement('h1');
+        title.textContent = this.title;
 
         /** @type {HTMLSpanElement} */
         const spanNumero = document.createElement('span');
@@ -39,6 +44,7 @@ class Card {
         // Écouteur d'événement pour mettre à jour le texte lorsque la taille de l'écran change sinon ça ne change pas automatiquement
         window.addEventListener('resize', updateText);
 
+        div.appendChild(title);
         div.appendChild(spanNumero);
         div.appendChild(spanSize);
 
@@ -50,15 +56,15 @@ class Card {
 /** @type {Card[]} */
 // Un tableau d'object de card
 const cards = [
-    new Card('1', '200px / 200px'),
-    new Card('2', '200px / 200px'),
-    new Card('3', '200px / 200px'),
-    new Card('4', '200px / 200px'),
-    new Card('5', '200px / 200px'),
-    new Card('6', '200px / 200px'),
-    new Card('7', '200px / 200px'),
-    new Card('8', '200px / 200px'),
-    new Card('9', '200px / 200px'),
+    new Card('title', '1', '200px / 200px'),
+    new Card('title','2', '200px / 200px'),
+    new Card('title','3', '200px / 200px'),
+    new Card('title','4', '200px / 200px'),
+    new Card('title','5', '200px / 200px'),
+    new Card('title','6', '200px / 200px'),
+    new Card('title','7', '200px / 200px'),
+    new Card('title','8', '200px / 200px'),
+    new Card('title','9', '200px / 200px'),
 ];
 
 /** @type {HTMLElement} */
@@ -85,4 +91,10 @@ toggleButton.addEventListener('click', () => {
     //Toggle couleur pour le bouton
     toggleButton.classList.toggle('bg-pink-500');
     toggleButton.classList.toggle('bg-black');
+});
+
+const hideBtn = document.getElementById('hideButton');
+hideBtn.addEventListener('click', () => {
+    cartes_container.classList.toggle('hidden');
+    cartes_container.classList.toggle('grid');
 });
