d7f9f670de949f742663380a23aee234035e5e35
alicia ross <ali.ross14@outlook.com>
2024-07-16 19:42:47 -0400

    essaie

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import { resolve } from 'path'
 export default {
     root: resolve(__dirname, 'src'),
     build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+        input: {
+        index: './src/index.html',
+        chien: './src/chien.html',
+        about: './src/apropos.html',
+        contact: './src/contact.html',
+        }
+    }
     },
     server: {
         port: 8080
